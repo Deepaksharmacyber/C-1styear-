04f82d3d38fe55dc32db0586a261b1e80ceca04b
deepaksharmacyber <deepaksharma909046@gmail.com>
2021-01-25 00:54:46 -0800

    evenodd

---
+++ EVEN-ODD.C
@@ -0,0 +1,15 @@
+//wap to check whether a number is even or odd
+#include<stdio.h>
+#include<conio.h>
+ void main()
+{
+ int n;
+ clrscr();
+ printf("\n enter a number");
+ scanf("%d",&n);
+ if(n%2 ==0)
+  printf("\n even number");
+  else
+  printf("\n odd number");
+  getch();
+ }
