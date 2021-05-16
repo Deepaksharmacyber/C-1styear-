8982b8be1ea0390da26f260389b6172502a009d0
Nitin Sharma <workonideas@gmail.com>
2021-05-16 17:28:17 +0800

    converted to the table format

--- basic_div.c
+++ basic_div.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 
 int main(){
-    int number = 0, divisor = 0, quotent = 1, product = 0;
+    int number = 0, divisor = 0, quotent = 0, product = 0, difference = 0;
 
     printf("enter the number to divide:");
     fflush(stdout);
@@ -14,10 +14,13 @@ int main(){
     scanf("%d", &divisor);
     printf("%d is the divisor\n", divisor);
 
-    while(number - product > divisor){
+    do{
         quotent = quotent + 1;
         product = quotent * divisor;
-    }
-    printf("quotent is %d", quotent);
+        difference = number - product;
+        printf("quotent:%d product:%d number:%d difference:%d\n",
+             quotent,product, number, difference);
+    }while(difference >= divisor);
+    printf("answer is %d\n", quotent);
     return 0;
 }
