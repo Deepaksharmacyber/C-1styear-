c2550a5eef990dc2615792506aae86f52a68750a
Nitin Sharma <workonideas@gmail.com>
2023-02-12 17:10:54 +0800

    improving the solution

--- GrossSalary.cpp
+++ GrossSalary.cpp
@@ -1,33 +1,24 @@
-#include <bits/stdc++.h>
+#include <iomanip>
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main() {
-	int t;
-	vector<int> salaries;
+	int t ,salary;
 	cin>>t;
 	float HRA ,DA ,Gross_salary ;
-	// receive input
 	while(t--){
-		int salary;
-		cin >> salary;
-		salaries.push_back(salary);
-	}
-	t = salaries.size();
-	for (int index = 0; index < t; index++){
-		int salary = salaries[index];
+	    cin>>salary;
 	    if (salary<1500){
-	        HRA =(float(10*salary)/100);
-	        DA = (float(90*salary)/100);
+	        HRA = float(10*salary)/100;
+	        DA = float(90*salary)/100;
 	        Gross_salary =(salary + HRA + DA) ;
-	        cout<<fixed<<setprecision(2)<<Gross_salary<<endl;
+	        cout<<fixed << setprecision(2) << Gross_salary<<endl;
 	    }
 	    else {
 	        HRA = 500;
-	        DA = (float(98*salary)/100);
+	        DA = float(98*salary)/100;
 	        Gross_salary = (salary + HRA + DA) ;
-	        cout<<fixed<<setprecision(2)<<Gross_salary<<endl;
+	        cout<<fixed << setprecision(2) << Gross_salary<<endl;
 	    }
 	}
 	return 0;
