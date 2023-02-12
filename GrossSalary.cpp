4ccd7b20bd5ba8e7bd2dc6a01ec20572abd9e73c
Nitin Sharma <workonideas@gmail.com>
2023-02-12 16:05:31 +0800

    copied wrong code

--- GrossSalary.cpp
+++ GrossSalary.cpp
@@ -1,22 +1,31 @@
 #include <bits/stdc++.h>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-	int t ,salary;
+	int t;
+	vector<int> salaries;
 	cin>>t;
 	float HRA ,DA ,Gross_salary ;
+	// receive input
 	while(t--){
-	    cin>>salary;
+		int salary;
+		cin >> salary;
+		salaries.push_back(salary);
+	}
+	t = salaries.size();
+	for (int index = 0; index < t; index++){
+		int salary = salaries[index];
 	    if (salary<1500){
-	        HRA =((10*salary)/100);
-	        DA = ((90*salary)/100);
+	        HRA =(float(10*salary)/100);
+	        DA = (float(90*salary)/100);
 	        Gross_salary =(salary + HRA + DA) ;
 	        cout<<fixed<<setprecision(2)<<Gross_salary<<endl;
 	    }
 	    else {
 	        HRA = 500;
-	        DA = ((98*salary)/100);
+	        DA = (float(98*salary)/100);
 	        Gross_salary = (salary + HRA + DA) ;
 	        cout<<fixed<<setprecision(2)<<Gross_salary<<endl;
 	    }
