5d041deffccd676b74f7fa1702ce6e041368d269
deepaksharmacyber <deepaksharma909046@gmail.com>
2023-02-15 10:21:44 +0530

    Completed

---
+++ Battery_health.cpp
@@ -0,0 +1,21 @@
+
+//https://www.codechef.com/problems/BTRYHLTH
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t , x ;
+	cin>>t;
+	while(t--){
+	    cin>>x;
+	    if (x>=80){
+	        cout<<"yes"<<endl;
+	    }
+	    else {
+	        cout<<"no"<<endl;
+	    }
+	}
+	return 0;
+}
