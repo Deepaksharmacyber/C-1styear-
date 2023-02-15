60272e8408af8b5ee638af9e65ae084ad4e08eec
deepaksharmacyber <deepaksharma909046@gmail.com>
2023-02-15 22:20:10 +0530

    solved

---
+++ game_between_friends.cpp
@@ -0,0 +1,36 @@
+//https://www.codechef.com/problems/FRGAME
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a, b, c, d;
+	    cin>>a>>b>>c>>d;
+	    if(a<b){
+	       a=a+c;
+	    }
+	    else{
+	        b=b+c;
+	    }
+	     if(a<b){
+	       a=a+d;
+	    }
+	    else{
+	        b=b+d;
+	    }
+	    if(a<b){
+	        cout<<"S"<<endl;
+	        
+	    }
+	    else{
+	        cout<<"N"<<endl;
+	    }
+	    
+	    
+	}
+	return 0;
+}
