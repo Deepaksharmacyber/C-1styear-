51f484c37231900ecda9b142a70b4ffe1d0ff536
deepaksharmacyber <deepaksharma909046@gmail.com>
2023-02-16 23:22:48 +0530

    solved

---
+++ numbers_pyramid.cpp
@@ -0,0 +1,18 @@
+//wap to print numbers pyramaid
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n ; 
+    cin>>n;
+    for(int i =1;i<=n;i++){
+        for(int j=0;j<i;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
