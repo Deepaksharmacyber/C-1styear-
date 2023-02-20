21f4124033101a7a875005faa6cfc90c610f1d18
deepaksharmacyber <deepaksharma909046@gmail.com>
2023-02-20 18:27:34 +0530

    solved

---
+++ numbers_pattern.cpp
@@ -0,0 +1,34 @@
+//wap to print numbers pattern in c++
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n ,a,b=0;
+    cin>>n;
+    a = n-1;
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<a;j++){
+            cout<<" ";
+        }
+        int count = 1;
+        for(int k=0;k<(i+b);k++){
+            if(k%2==0){
+                cout<<count++;
+            }
+            else {
+                cout<<" ";
+            }
+        }
+        b = b+1;
+        for(int j=0;j<a;j++){
+            cout<<" ";
+        }
+        a = (a-1);
+        cout<<endl;
+    }
+
+    return 0;
+}
