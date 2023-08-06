b34b254a5f08740d4f6c8b8938e285577a21d702
deepaksharmacyber <deepaksharma909046@gmail.com>
2023-08-06 13:04:41 +0530

    pdf_to_docs_docs_to_pdf

--- py_train/pdf_to_docs_to_pdf.py
+++ py_train/pdf_to_docs_to_pdf.py
@@ -1,8 +1,16 @@
-from pdf2docx import Converter
+# from pdf2docx import Converter
 
-old_pdf ="""D:\dsa\dsa1.pdf.pdf"""
-new_doc = 'D:\\dsa\\new.docx'
+# old_pdf = "dsa1.pdf.pdf"
+# new_doc = 'D:\\dsa\\new.docx'
 
-obj = Converter(old_pdf)
-obj.convert(new_doc)
-obj.close()
+# obj = Converter(old_pdf)
+# obj.convert(new_doc)
+# obj.close()
+
+#"""D:\dsa\dsa1.pdf.pdf"""
+#'D:\\dsa\\new.docx'
+
+
+from docx2pdf import convert
+
+convert("new.docx",'D:\\dsa\\new_pdf.pdf')
