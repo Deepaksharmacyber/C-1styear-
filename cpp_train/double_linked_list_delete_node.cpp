5bd3c9e05533ac3aeef7f87aa10ab041fc6a9d76
Deepaksharmacyber <deepaksharma909046@gmail.com>
2023-08-19 00:52:32 +0530

    double_linked_list_delete_node.cpp

---
+++ cpp_train/double_linked_list_delete_node.cpp
@@ -0,0 +1,71 @@
+//wap to delete a node in double linked list 
+
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* prev;
+};
+
+void deleteNode(Node** head_ref,Node* del){
+    if(*head_ref==NULL || del==NULL){
+        return ;
+    }
+
+    if(*head_ref==del){
+        *head_ref=del->next;
+    }
+
+    if(del->next!=NULL){
+        del->next->prev=del->prev;
+    }
+
+    if(del->prev!=NULL){
+        del->prev->next=del->next;
+    }
+    free(del);
+    return ;
+}
+
+void push(Node** head_ref,int new_data){
+    Node* new_node = new Node();
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    new_node->prev = NULL;
+
+    if(*head_ref!=NULL){
+        (*head_ref)->prev=new_node;
+    }
+
+    *head_ref=new_node;
+}
+
+void printList(Node* node){
+    while(node!=NULL){
+        cout<<node->data<<" ";
+        node = node->next;
+    }
+}
+
+int main(){
+    Node* head = NULL;
+    push(&head,2);
+    push(&head,4);
+    push(&head,8);
+    push(&head,10);
+
+    cout<<"linked list before deletion ";
+    printList(head);
+    
+    cout<<"\n";
+    
+    deleteNode(&head,head);
+    deleteNode(&head,head->next);
+    deleteNode(&head,head->next);
+
+    cout<<"linked list after deletion ";
+    printList(head);
+}
